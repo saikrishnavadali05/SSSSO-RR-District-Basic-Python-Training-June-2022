9c3e40b385f3467c2e51fe79deb9814b6dbfb755
Shruti Reddi <107172257+ShrutiReddi02@users.noreply.github.com>
2022-06-20 11:03:47 +0530

    Add files via upload

---
+++ 005ShrutiReddi/demo_7.py
@@ -0,0 +1,4 @@
+# Python program to convert meters(m) into centimeters (cm)
+m = input(" Enter meter: ")
+cm = int(m)*100
+print("cm", cm)python demo_7.py
---
+++ 005ShrutiReddi/slicing.py
@@ -0,0 +1,5 @@
+my_list = "Cython"
+#print(my_list[::-3])
+
+#to replace C by P in the string
+print(my_list.replace("C","P"))
