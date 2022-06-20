4dcf8c922688f8ee078be313b4d48e731e35f192
Snehadeshpande01 <98334365+Snehadeshpande01@users.noreply.github.com>
2022-06-20 17:43:20 +0530

    Create prg2.py

---
+++ prg2.py
@@ -0,0 +1,8 @@
+#problem statement:program to find the maximum, minimum, average, addition, multiplication of any 2 integer numbers
+n1=int(input("enter number1:"))
+n2=int(input("enter number2:"))
+print("maximum :",max(n1,n2))
+print("minimum :",min(n1,n2))
+print("average :",(n1+n2)/2)
+print("addition :",(n1+n2))
+print("multipication :",(n1*n2))
