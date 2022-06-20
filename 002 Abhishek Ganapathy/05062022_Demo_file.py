9c769a343f38da13b3cb18bc9d07d1c2852c2c3e
Vissamsetty-Bharathrath <101404300+Vissamsetty-Bharathrath@users.noreply.github.com>
2022-06-20 20:50:18 +0530

    Delete 002 Abhishek Ganapathy directory

--- 002 Abhishek Ganapathy/05062022_Demo_file.py
+++
@@ -1,1 +0,0 @@
-print("On Sri Sairam")
--- 002 Abhishek Ganapathy/1st Python program.py
+++
@@ -1,4 +0,0 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-print("Hello")
-Hello
