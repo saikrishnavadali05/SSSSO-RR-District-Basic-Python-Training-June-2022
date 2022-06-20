02c3b002442f0133e3b8c4911c118fe98dd6128b
Badrinath777 <107308157+Badrinath777@users.noreply.github.com>
2022-06-20 22:41:52 +0530

    118 Badrianth contributions

---
+++ 118_badrinath_contributions/excercise_5_program_5.py
@@ -0,0 +1,5 @@
+student_score = {'Charan': 10,
+                 'Chetan':20,
+                 'Ajith':30,
+                 'Kumar':50}
+print(student_score)
---
+++ 118_badrinath_contributions/excercise_7_program_7.py
@@ -0,0 +1,6 @@
+print(
+   "*\n"
+ "*python*\n"
+"*is*a* \n"
+"*good *programming*language*\n"
+"*to*learn*for*beginners*")
