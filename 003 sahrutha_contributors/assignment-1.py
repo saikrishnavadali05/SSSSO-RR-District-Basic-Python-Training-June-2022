afe01f5b622d3c625f409982276075980553b262
Vadali Sai Krishna <saikrishnavadali05@gmail.com>
2022-06-21 21:43:49 +0530

    Delete 003 sahrutha_contributors directory

--- 003 sahrutha_contributors/assignment-1.py
+++
@@ -1,4 +0,0 @@
-w = input()
-list = w.split( )
-for r in list:
-    print(r, '\t')
--- 003 sahrutha_contributors/assignment-2.py
+++
@@ -1,9 +0,0 @@
-a=int(input())
-b=int(input())
-c=a+b
-d=a*b
-print(max(a,b))
-print(min(a,b))
-print(c/2)
-print(c)
-print(d)
