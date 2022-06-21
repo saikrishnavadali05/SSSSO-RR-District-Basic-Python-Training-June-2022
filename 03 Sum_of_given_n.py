f1fabd4cd899af15372d16d752ab6f9f7249b418
Padigela Ranjith <89335247+ranjithpadigela@users.noreply.github.com>
2022-06-21 14:48:24 +0530

    Delete 03 Sum_of_given_n.py

--- 03 Sum_of_given_n.py
+++
@@ -1,8 +0,0 @@
-#Write a program that accepts an integer (n) and 
-# computes the value of n+nn+nnn. 
-# If n is 5, the output should be 5+55+555 = 615
-n = input("Enter the number:")
-first = n
-second = n+n
-third = n+n+n
-print("The given input is it after computing the n+nn+nnn result is",int(first) + int(second) + int(third))
