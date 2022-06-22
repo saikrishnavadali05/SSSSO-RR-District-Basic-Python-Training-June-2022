f6f8a8c29703bec0094c3de248fbe21b4e675069
VarunBabuRayabarapu <107998972+VarunBabuRayabarapu@users.noreply.github.com>
2022-06-22 17:26:18 +0530

    Add files via upload

---
+++ 019_Varun_Rayabarapu_Contributions/1Assignment-9.py
@@ -0,0 +1,5 @@
+#Write a program that imports numpy, matplotlib, pandas libraries. If the program imports those modules successfully. print "modules imported successfully"
+import numpy
+import matplotlib
+import pandas
+print("modules imported successfully")
