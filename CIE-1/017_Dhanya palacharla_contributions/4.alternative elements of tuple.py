27a3e4e8d7f0e0eaeb2a8ca66b18d4398cded263
Vissamsetty-Bharathrath <101404300+Vissamsetty-Bharathrath@users.noreply.github.com>
2022-06-22 20:39:15 +0530

    Delete CIE-1 directory

--- CIE-1/017_Dhanya palacharla_contributions/4.alternative elements of tuple.py
+++
@@ -1,9 +0,0 @@
-list=[]
-n=int(input("enter the no. of elements"))
-for i in range(0,n):
-    elements=int(input())
-    list.append(elements)
-tupl=tuple(list)
-print("elements in tuple are:",tupl)
-print("alternative elements in tuple are:",tupl[ : :2])
-  
--- CIE-1/017_Dhanya palacharla_contributions/7.pattern.py
+++
@@ -1,6 +0,0 @@
-print("\t","   *")
-print("\t* python *")
-print("     *  is  *  a    *")
-print("  * good * programming * language *")
-print("*  to *  learn *  for  *  beginners  *")
-
