3ec7b0936acea28eda55a73f88bb3ca87f8d44a8
Vissamsetty-Bharathrath <101404300+Vissamsetty-Bharathrath@users.noreply.github.com>
2022-06-22 20:40:57 +0530

    Delete 015_sahrutha_contributions directory

--- 015_sahrutha_contributions/015_sahrutha_contributions/casechange.py
+++
@@ -1,9 +0,0 @@
-#To convert the string into uppercase
-
-a=input()
-print(a.upper())
-
-#To convert the string into lowercase
-
-a=input()
-print(a.lower())
--- 015_sahrutha_contributions/swap_with_3rdvariable.py
+++
@@ -1,9 +0,0 @@
-#Code for swapping of two numbers with using third variable
-
-a=int(input())
-b=int(input())
-c=a
-a=b
-b=c
-print("a:",a)
-print("b:",b)
