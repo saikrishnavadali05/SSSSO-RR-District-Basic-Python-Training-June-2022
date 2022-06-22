07796988a9bc4d5f43f67b8d714ba0bd6a5e164b
Vissamsetty-Bharathrath <101404300+Vissamsetty-Bharathrath@users.noreply.github.com>
2022-06-22 20:44:06 +0530

    Add files via upload

---
+++ 017_Dhanya palacharla_contributions/CIE-1/2.operations.py
@@ -0,0 +1,11 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+if a>b:
+    print("maximum value is:",a)
+    print("minimum value is:",b)
+else:
+    print("maximum value is:",b)
+    print("minimum value is:",a)
+print("average of two numbers is:",(a+b)/2)
+print("addition of two numers is:",a+b)
+print("multiplication of two numers is:",a*b)
