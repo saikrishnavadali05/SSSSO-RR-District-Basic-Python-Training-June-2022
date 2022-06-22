f1d4a498b202ad9b368b05734a6c1925a1ecb6af
NageshwariPadegela <107258033+NageshwariPadegela@users.noreply.github.com>
2022-06-22 09:46:55 +0530

    Add files via upload

---
+++ 007_NageshwariPadegela_Condributions/Exercise - 1/05.py
@@ -0,0 +1,10 @@
+# write a word in the for the variable and print it (short answer)
+'''
+planet_near_to_sun=
+print()
+'''
+from numpy import place
+
+
+planet_near_to_sun = "Mercury"
+print("The neartest planet to sun is %s"%planet_near_to_sun)
