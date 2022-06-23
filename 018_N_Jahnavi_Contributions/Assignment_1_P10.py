68dbf0fa1c3e3ac03108157c2d25675d48c3ef93
N Jahnavi <106874803+jahnavi0219@users.noreply.github.com>
2022-06-23 17:10:09 +0530

    Add files via upload

---
+++ 018_N_Jahnavi_Contributions/Assignment_1_P10.py
@@ -0,0 +1,28 @@
+# # Zen of Python:A Pythoneer once wrote 20 aphorisms stating how one can write beautiful and clean code with Python. 
+# Came to be known as “The Zen of Python”, these aphorisms exploded amongst Python developers.
+# Tim Peters wrote these BDFL’s (Benevolent Dictator For Life, a nickname of Python creator Guido van Rossum) 20 guiding principles for Python’s design but the last aphorism was left for van Rossum to fill in.
+#Till today, there are 19 aphorisms as Rossum said that the last aphorism is “some bizarre Tim Peters in-joke.”
+
+# The Zen of Python, by Tim Peters
+# 
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+
+import this
