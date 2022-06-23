87d4f88ce62b7488eba2e07fb1c0a2e27bf4a7a8
N Jahnavi <106874803+jahnavi0219@users.noreply.github.com>
2022-06-23 23:06:36 +0530

    Add files via upload

--- 018_N_Jahnavi_Contributions/Assignment_1_P10.py
+++ 018_N_Jahnavi_Contributions/Assignment_1_P10.py
@@ -4,25 +4,28 @@
 #Till today, there are 19 aphorisms as Rossum said that the last aphorism is “some bizarre Tim Peters in-joke.”
 
 # The Zen of Python, by Tim Peters
-# 
-# Beautiful is better than ugly.
-# Explicit is better than implicit.
-# Simple is better than complex.
-# Complex is better than complicated.
-# Flat is better than nested.
-# Sparse is better than dense.
-# Readability counts.
-# Special cases aren't special enough to break the rules.
-# Although practicality beats purity.
-# Errors should never pass silently.
-# Unless explicitly silenced.
-# In the face of ambiguity, refuse the temptation to guess.
-# There should be one-- and preferably only one --obvious way to do it.
-# Although that way may not be obvious at first unless you're Dutch.
-# Now is better than never.
-# Although never is often better than *right* now.
-# If the implementation is hard to explain, it's a bad idea.
-# If the implementation is easy to explain, it may be a good idea.
-# Namespaces are one honking great idea -- let's do more of those!
+import this # This itself prints the entire zen python 
 
-import this
+# For exclusive illustration:-
+print("\t")
+
+print("Beautiful is better than ugly.")                                            
+print(" Explicit is better than implicit")
+print(" Simple is better than complex.")
+print(" Complex is better than complicated.")
+print(" Flat is better than nested.")
+print(" Sparse is better than dense.")
+print(" Readability counts.")
+print(" Special cases aren't special enough to break the rules.")
+print(" Although practicality beats purity.")
+print(" Errors should never pass silently.")
+print(" Unless explicitly silenced.")
+print(" In the face of ambiguity, refuse the temptation to guess.")
+print(" There should be one-- and preferably only one --obvious way to do it.")
+print(" Although that way may not be obvious at first unless you're Dutch.")
+print(" Now is better than never.")
+print(" Although never is often better than *right* now.")
+print(" If the implementation is hard to explain, it's a bad idea.")
+print(" If the implementation is easy to explain, it may be a good idea.")
+print(" Namespaces are one honking great idea -- let's do more of those!")
+
