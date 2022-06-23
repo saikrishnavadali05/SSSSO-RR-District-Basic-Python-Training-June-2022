105f62694611fc6dd1694bef7f4d42d2154cf389
srikanth-srichakra <108064471+srikanth-srichakra@users.noreply.github.com>
2022-06-23 16:23:50 +0530

    Add files via upload

---
+++ 023_Srikanth_Srichakra_Contribution/print_a_pattern.py
@@ -0,0 +1,14 @@
+''' 7.	Write a program to print this pattern (hint : use \t and \n wherever required).
+         *
+     * python *
+   * is  *  a    *
+ * good  * programming * language *
+* to * learn * for * beginners *
+'''
+print("\t    *")
+print("\t* python *")
+print("     * is  *  a    *")
+print("  * good  * programming * language *")
+print("* to * learn * for * beginners *")
+
+print("")
