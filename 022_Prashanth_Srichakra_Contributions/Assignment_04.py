b8b3fca5f438e6e8c71a8fb1fc625616540c3459
Vissamsetty-Bharathrath <101404300+Vissamsetty-Bharathrath@users.noreply.github.com>
2022-06-23 21:57:04 +0530

    Delete 022_Prashanth_Srichakra_Contributions directory

--- 022_Prashanth_Srichakra_Contributions/Assignment_04.py
+++
@@ -1,6 +0,0 @@
-# Write a program to print all the alternative elements of a tuple. The elements to the tuple have to be given to the program using input() function.
-tup = tuple(input("Enter tuple elements:"))
-print("Alternate elements in the tuple are:")
-alt_tup = tup[::2]
-for i in alt_tup:
-    print(i, end=" ")
