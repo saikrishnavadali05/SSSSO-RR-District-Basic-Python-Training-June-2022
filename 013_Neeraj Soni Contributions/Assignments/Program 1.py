eb9e7f6adca64cef6c36908659d5fd309c132efc
nirajsoni140 <107300527+nirajsoni140@users.noreply.github.com>
2022-06-23 16:38:17 +0530

    Add files via upload

---
+++ 013_Neeraj Soni Contributions/Assignments/Program 1.py
@@ -0,0 +1,13 @@
+#Assignment 
+from re import L
+
+
+print(1)
+Sen = ['Love', 'All', 'Serve', 'All', 'Help', 'Ever', 'Hurt', 'Never']
+for v in Sen:
+    print(v)
+
+print( )
+sentence = ('Love','All','Serve','All')
+while (sentence < 9):
+    print(Sen)
