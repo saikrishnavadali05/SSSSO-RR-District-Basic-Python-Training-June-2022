7c86b7b3dc592574e8b8053b60613906f5a5623c
Vissamsetty-Bharathrath <101404300+Vissamsetty-Bharathrath@users.noreply.github.com>
2022-06-24 09:15:29 +0530

    022_KethaDhanaVeeraChaitanya_contributions

---
+++ 022_KethaDhanaVeeraChaitanya_contributions/program3.py
@@ -0,0 +1,2 @@
+list=[62,17,1,6,81,716,'sairam',56.61,6]
+print('the list in reverse order is:',list[::-1])
---
+++ 022_KethaDhanaVeeraChaitanya_contributions/program9.py
@@ -0,0 +1,26 @@
+The Zen of Python is a collection of 19 "guiding principles" for writing computer programs that influence the design of the Python programming language. Software engineer Tim Peters wrote this set of principles and posted it on the Python mailing list in 1999.
+
+example found on google is :
+>>> import this
+
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than right now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
