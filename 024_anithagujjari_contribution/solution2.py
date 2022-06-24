6dfbbd3aed074eb67dda3d44913a56d50c6f6038
Vissamsetty-Bharathrath <101404300+Vissamsetty-Bharathrath@users.noreply.github.com>
2022-06-24 09:14:15 +0530

    Delete 024_anithagujjari_contribution directory

--- 024_anithagujjari_contribution/solution2.py
+++
@@ -1,7 +0,0 @@
-number1=int(input("enter number1:"))
-number2=int(input("enter number2:"))
-print("maximum :",max(number1,number2))
-print("minimum :",min(number1,number2))
-print("average :",(number1+number2)/2)
-print("addition :",(number1+number2))
-print("multipication :",(number1*number2))
--- 024_anithagujjari_contribution/solution4.py
+++
@@ -1,5 +0,0 @@
-n=input("enter elements seperated by ,")
-list1=n.split(',')
-tuple1=tuple(list1)
-for i in range(0,len(tuple1),2):
-    print(tuple1[i])
