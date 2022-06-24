20a41ab93e96be706f76b659d2d3d00e872e092f
Vissamsetty-Bharathrath <101404300+Vissamsetty-Bharathrath@users.noreply.github.com>
2022-06-24 08:59:04 +0530

    Delete 021_Prashanth_Srichakra_Contributions directory

--- 021_Prashanth_Srichakra_Contributions/Zen_Of_Python.py
+++
@@ -1,8 +0,0 @@
-# The Zen of Python are a few guidelines
-# for writing computer programs in python programming language
-# Tim Peters wrote these guidelines
-# These guidelines influence the design of python programs
-
-# The following import statement is a hidden easter egg
-# It prints all the Zen of Python written by Tim Peters
-import this
