2fe8e2f6dbf0c521f0ed22b8f105ca1756edde56
divyaguthikonda <107299927+divyaguthikonda@users.noreply.github.com>
2022-06-25 12:35:45 +0530

    042 DivyaGuthikonda Contributions

---
+++ 042_DivyaGuthikonda_Contributions/demo2.py
@@ -0,0 +1,22 @@
+
+print("hi")
+print("hello world")
+#hi
+
+a=10
+b=20
+print(a+b)
+#hi
+#hello world
+#30
+
+a=10
+b=20
+c=a+b
+print(c) # 30
+
+a=10
+b=20
+c=a+b
+print(a,"+",b,"=",c) # 10 + 20 = 30
+
