a59532eb322a48832a6cb1cc65c38f67172a0047
NageshwariPadegela <107258033+NageshwariPadegela@users.noreply.github.com>
2022-06-28 20:11:40 +0530

    Add files via upload

--- 007_NageshwariPadegela_Condributions/Exercise - 10/01.py
+++ 007_NageshwariPadegela_Condributions/Exercise - 10/01.py
@@ -1,1 +1,11 @@
+# Write a function that take string as a parameter. the string is given by the user as input. the final output from the function is to reverse the string.
+'''
+ example the string is Multiple -
+  output is elpitluM
+'''
+mystring = input('Enter a string of your choice: ')
+def reverse(x):
+    return x[::-1]
+print('Reverse of a string is: ', reverse(mystring))
 
+    
