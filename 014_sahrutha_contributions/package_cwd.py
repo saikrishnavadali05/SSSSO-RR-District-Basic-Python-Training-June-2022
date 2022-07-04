82ff28e1ce0931f1bebec03115941201f45c56f7
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2022-07-04 23:32:30 +0530

    014_sahrutha_c

---
+++ 014_sahrutha_contributions/package_cwd.py
@@ -0,0 +1,7 @@
+'''
+os.getcwd()
+It returns the current working directory(CWD) of the file
+'''
+
+import os     
+print(os.getcwd())     
---
+++ 014_sahrutha_contributions/package_math.py
@@ -0,0 +1,6 @@
+#math.log()
+''' This method returns the natural logarithm of a given number. It is calculated to the base e '''
+
+import math    
+number = 2e-7  # small value of of x    
+print('log(fabs(x), base) is :', math.log(math.fabs(number), 10))  
