6788164093af879d9a1ab2f249218579013a2aff
DIVYA GUTHIKONDA <107299927+divyaguthikonda@users.noreply.github.com>
2022-07-08 16:53:40 +0530

    exercise16 was uploaded

--- 042_DivyaGuthikonda_Contributions/exercise16.py
+++ 042_DivyaGuthikonda_Contributions/exercise16.py
@@ -1,2 +1,10 @@
 #Write a script for multiplication of two numbers(int or float) and the input is taken from the user. When the user gives strings the error should be handled
-n=int(input("Give two numbers: "))
+from sys import argv
+try:
+    a=int(input("Give first number: "))
+    b=float(input("Give first number: "))
+    multiply=a*b
+    print(multiply)
+except:
+    print("Error: Provide two numbers.")
+    
