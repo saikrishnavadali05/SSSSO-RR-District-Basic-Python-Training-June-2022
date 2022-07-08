60abda0b29ca6ee57edb76ffe0af944a39651477
DIVYA GUTHIKONDA <107299927+divyaguthikonda@users.noreply.github.com>
2022-07-08 16:57:34 +0530

    comments are added in exercise16

--- 042_DivyaGuthikonda_Contributions/exercise16.py
+++ 042_DivyaGuthikonda_Contributions/exercise16.py
@@ -7,4 +7,17 @@ try:
     print(multiply)
 except:
     print("Error: Provide two numbers.")
-    
+
+#(two inputs are integers)
+#  Give first number: 12
+#  Give first number: 15
+#  180.0
+
+#(one input is integer and other is a float)
+#Give first number: 26
+#Give first number: 25.256
+#656.656
+
+#(if we enter any strinng)
+#Give first number: hi
+#Error: Provide two numbers.
