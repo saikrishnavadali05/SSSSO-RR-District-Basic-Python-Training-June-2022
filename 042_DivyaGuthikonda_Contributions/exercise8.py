851a6d612f05abc4cdd06385f43090297086966c
DIVYA GUTHIKONDA <107299927+divyaguthikonda@users.noreply.github.com>
2022-07-08 13:56:03 +0530

    Exercise 7 was deleted to see the changes

--- 042_DivyaGuthikonda_Contributions/exercise8.py
+++ 042_DivyaGuthikonda_Contributions/exercise8.py
@@ -25,6 +25,7 @@ print("Factorial of Given Number is :", f)
 #Factorial of Given Number is : 120
 
 #Write a script to print the count of number of digits within the given input number using while loop
+n=int(input("Enter Number : "))
 count=0
 while(n>0):
     count=count+1
