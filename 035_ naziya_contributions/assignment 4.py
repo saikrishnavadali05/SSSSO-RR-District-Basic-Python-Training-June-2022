2288e0b69450b577a1f08b461843bfb0c47b7beb
Naziya674 <107803844+Naziya674@users.noreply.github.com>
2022-07-11 22:55:55 +0530

    practice

---
+++ 035_ naziya_contributions/assignment 4.py
@@ -0,0 +1,4 @@
+#program to print all the alternative elements of a tuple
+tuple = input("enter the values :",)
+print("alternative elements in a tuple :", tuple)
+print("alternative elements :", tuple[::2])
