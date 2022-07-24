151be3f098863ba7bfdf6e14032247142f0113a8
Prashanth Srichakra <107780707+Prashanth-Srichakra@users.noreply.github.com>
2022-07-24 16:05:48 +0530

    Update Birthday_Calculator.py

--- 029_Prashanth_Srichakra_Contributions/Birthday_Calculator.py
+++ 029_Prashanth_Srichakra_Contributions/Birthday_Calculator.py
@@ -22,13 +22,7 @@ user_date = input("Enter date in DDMMYYYY format please: ")
 if len(user_date)!=8:
     print("Enter a valid date please!")
 else:
-    user_year = int(user_date[4:8])
-    user_mon = int(user_date[2:4])
-    user_day = int(user_date[0:2])
-    user_date = dt.date(user_year, user_mon, user_day)
-
     user_bday = user_date - today
-    days = user_bday.total_seconds() // 86400
-
     print("Countdown for birthday is", user_bday.days,"days!!!")
 
+    
