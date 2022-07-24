143343203929da12630106b1a957fd46c274d44e
Madhumitha-Namburi <107269883+Madhumitha-Namburi@users.noreply.github.com>
2022-07-24 15:31:02 +0530

    assignment

---
+++ 043_Madhumitha_contributions/Assignment-24-07-22/Add.py
@@ -0,0 +1,5 @@
+def Add(Digits,Even):
+    Sum = 0
+    for i in range(0,Digits):
+        Sum = int(Even[i]) + Sum
+    print("The sum is",Sum)
---
+++ 043_Madhumitha_contributions/Assignment-24-07-22/multiple.py
@@ -0,0 +1,5 @@
+def multple(Digits,Even):
+    Sum_of_Multiplication = 1
+    for i in range(0,Digits):
+        Sum_of_Multiplication = int(Even[i]) * Sum_of_Multiplication
+    print("The multiplication is",Sum_of_Multiplication)
