d5c25fba9a58789e815e224c5145f4032dbcb84a
divyajyothi2022 <107297038+divyajyothi2022@users.noreply.github.com>
2022-07-24 13:39:32 +0530

    06 Divyajyothi contributions

---
+++ 06_Divyajyothi_contribution_Assgment2/Aprgm13.py
@@ -0,0 +1,15 @@
+#13.Write a function that accepts a sentence from a paragraph stored in a text file input.
+# txt and calculates the number of letters and digits within that sentence string.
+
+f = open(r"C:\Users\Divya\Desktop\input_name.txt","r")
+text = f.read()
+digit=letter=0
+for char in text:
+    if char.isdigit():
+        digit=digit+1
+    elif char.isalpha():
+        letter=letter+1
+    else:
+        pass
+print("Letters:",letter)
+print("Digits:",digit)
