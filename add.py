dc6cd5ef937c7d88fcd793d8cbe06b11a90b9989
Dnyaneshwar Kharat <106876244+DNYANESHWARKHARAT@users.noreply.github.com>
2022-07-24 15:00:20 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,71 @@
+# Program make a simple calculator
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
+    else:
+        print("Invalid Input")
+
+        
+#1.Add
+#2.Subtract
+#3.Multiply
+#4.Divide
+#Enter choice(1/2/3/4): 5
+#Invalid Input
+#Enter choice(1/2/3/4): 4
+#Enter first number: 20
+#Enter second number: 5
+#20.0 / 5.0 = 4.0
+#Let's do next calculation? (yes/no):
+
+
+
+#a) Create different python files - add.py, multiple.py and write functions for these operations.
