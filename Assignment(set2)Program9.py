6a4d7e62a05bc2905aa5d97deac8a31db408f087
uday0097 <108031764+uday0097@users.noreply.github.com>
2022-07-24 08:09:47 +0530

    023_Uday.T_contributions

---
+++ Assignment(set2)Program9.py
@@ -0,0 +1,11 @@
+#9.Store student name, id, age, class, course in a dictionary. Write a script to lookup a student name, age, class, course given the student ids.
+student_details = {"name": "uday", "id": 482,
+                   "age": 27, "class": "btech final year", "course": "ECE"}
+id_no = int(input("Enter id:"))
+if student_details["id"] == id_no:
+    print("Name:", student_details["name"])
+    print("Age:", student_details["age"])
+    print("Class:", student_details["class"])
+    print("Course:", student_details["course"])
+else:
+    print("Entered id is not present in the data,please enter valid id no THANK YOU ")
