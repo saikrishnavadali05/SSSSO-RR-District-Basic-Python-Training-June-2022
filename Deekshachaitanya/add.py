37dc45c9e9fe6a62fd9505274eefff2b6cf932fd
Deekshachaitanya28 <109900448+Deekshachaitanya28@users.noreply.github.com>
2022-07-25 00:00:17 +1300

    Update add.py

--- Deekshachaitanya/add.py
+++ Deekshachaitanya/add.py
@@ -1,1 +1,11 @@
-
+def AddEvenNumbers(num1, num2, num3, num4):
+    if(num1 % 2) != 0:
+        num1 = 0
+    if(num2 % 2) != 0:
+        num2 = 0
+    if(num3 % 2) != 0:
+        num3 = 0
+    if(num4 % 4) != 0:
+        num4 = 0
+    sum = num1+num2+num3+num4
+    return sum
