971ae53fddc6cf2c3569e1b7ee8b0c49153d6dae
Praveena <praveenakumbum10@gmail.com>
2025-05-23 23:45:23 +0530

    Week 1 Assignment_Variables

---
+++ may_2025_contributions/PY-F-118_Praveena_Submission/Assignment-Week1_Variables.py
@@ -0,0 +1,8 @@
+# Python Training 2025 - Variables 
+# Description : pyhton program to capture the first name, last name and later print the full anme
+# Author : Praveena Kumbum
+# Date : 20 May 2025
+
+first_name = "Praveena"
+last_name= "Kumbum"
+print(f"My full name is {first_name} {last_name}.")
