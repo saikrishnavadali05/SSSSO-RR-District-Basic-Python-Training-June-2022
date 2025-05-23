119b3acf5af876bf9b7447b2a9a92d6279116b68
Praveena <praveenakumbum10@gmail.com>
2025-05-23 23:39:42 +0530

    Removing the file since file name and content are updatde

--- may_2025_contributions/PY-F-118_Praveena_Submission/Variables.py
+++
@@ -1,8 +0,0 @@
-# Python Training 2025 - Variables 
-# Description : pyhton program to capture the first name, last name and later print the full anme
-# Author : Praveena Kumbum
-# Date : 20 May 2025
-
-a = "Praveena"
-b = "Kumbum"
-print(f"My full name is {a} {b}.")
