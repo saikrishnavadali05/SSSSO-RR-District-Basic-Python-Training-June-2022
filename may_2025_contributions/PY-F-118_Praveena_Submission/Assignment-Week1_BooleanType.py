2009ab207486e157e14861c61f9ae8941bbbaacb
Praveena <praveenakumbum10@gmail.com>
2025-05-23 23:52:24 +0530

    Week 1 - Boolean Type

---
+++ may_2025_contributions/PY-F-118_Praveena_Submission/Assignment-Week1_BooleanType.py
@@ -0,0 +1,11 @@
+# Python Training 2025 - Boolean Type
+# Description : Python program : Store 0, '', and 42 in a list. Loop through the list and print whether each element is truthy or falsy.
+# Author : Praveena Kumbum
+# Date : 23 May 2025
+
+My_List = [0,'',42]
+for ele in My_List:
+    if ele:
+        print("truthy.")
+    else:
+        print("Falsy.")
